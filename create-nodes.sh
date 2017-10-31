98b602d9fbfc18c5cbff4a974d9c2918afbfdc17
Kalidux <kalidux@gmail.com>
2017-11-01 00:15:15 +0100

    Update create-nodes.sh

--- create-nodes.sh
+++ create-nodes.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
 
-NUMBER_NODES=10
-LEADER_NODE=node6
+NUMBER_NODES=5
+LEADER_NODE=node1
 
 # create nodes
-for NODE in $(seq 6 $NUMBER_NODES)
+for NODE in $(seq 1 $NUMBER_NODES)
 do
   docker-machine rm node$NODE
   docker-machine create --driver virtualbox node$NODE
