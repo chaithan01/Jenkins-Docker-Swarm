a78b5a6438f5ab2d412e280bcfee5979d3d48743
Kalidux <kalidux@gmail.com>
2017-10-31 21:33:50 +0100

    Update workers-join.sh

--- workers-join.sh
+++ workers-join.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 # Other nodes join as workers
 for n in $(seq 2 $NUMBER_NODES); do
-  docker-machine ssh node-$n docker swarm join --token $JOIN_TOKEN $LEADER_IP:2377
+  docker-machine ssh node$n docker swarm join --token $JOIN_TOKEN $LEADER_IP:2377
 done;
