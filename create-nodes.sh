a185fbd20476d6e9a17ae76a07bd86fb78e5ceaa
Kalidux <kalidux@gmail.com>
2017-10-31 21:12:42 +0100

    Update create-nodes.sh

--- create-nodes.sh
+++ create-nodes.sh
@@ -4,9 +4,10 @@ NUMBER_NODES=5
 LEADER_NODE=node_1
 
 # create nodes
-for NODE in $(seq 1 $NUMBER_NODES); do
+for NODE in $(seq 1 $NUMBER_NODES)
+do
   echo node_$NODE
   docker-machine create --driver virtualbox node_$NODE
-done;
+done
 
 LEADER_IP=`docker-machine ip $LEADER_NODE`
